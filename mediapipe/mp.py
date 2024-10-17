4dac440fa65ce8f56f3377351d729449c618d005
seok <tjralsrud8@gmail.com>
2024-10-17 10:55:38 +0900

    mp.py 수정

--- mediapipe/mp.py
+++ mediapipe/mp.py
@@ -1,16 +1,7 @@
 import cv2
-import mediapipe as mp
 from mediapipe.python.solutions import hands as mp_hands
 from mediapipe.python.solutions import drawing_utils as mp_drawing
 import numpy as np
-
-def check():
-    w = np.random.randint(100, 1000, size=10)
-    h = np.random.randint(100, 600, size = 10)
-    return  set(map(tuple, np.column_stack((w, h))))
-whlist = check()
-
-# print(whlist)
 
 hands = mp_hands.Hands()
 
@@ -20,9 +11,6 @@ width = 1280
 height = 720
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-state = False
-cnt = 0
-save_idx = 0
 
 while cap.isOpened():
     point_x, point_y = None, None
@@ -39,24 +27,21 @@ while cap.isOpened():
     img = np.full((height,width,3), fill_value=255, dtype=np.uint8)
     
     
-    for w,h in whlist:
-        cv2.circle(img, (w,h), 30, (0,0,255), -1)
-    
     if result.multi_hand_landmarks:
         for hand_landmarks, hand_lr in zip(result.multi_hand_landmarks, result.multi_handedness) :
             mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             point_9 = hand_landmarks.landmark[9]
             point_x, point_y = int(hand_landmarks.landmark[9].x * width), (hand_landmarks.landmark[9].y * height)
             
-            for xy in whlist:
-                if (xy[0]-50 <= point_x <= xy[0]+50) and (xy[1]-50 <= point_y <= xy[1]+50):
-                    whlist.remove((xy[0], xy[1]))
-                    cnt += 1
-                    break
+            # 양쪽 손 다 인식
+            if hand_lr.classification[0].label == 'Left':
+                print({"lr": 'Left', "x": point_x, "y": point_y})
+            else:
+                print({"lr": 'Right', "x": point_x, "y": point_y})
+            
 
             
-    print({"cnt":cnt, "x": point_x, "y": point_y})       
-    cv2.putText(img, str(cnt), (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,0,0), 5)     
+        # print({"lr": hand_lr.classification[0].label, "x": point_x, "y": point_y}) # 먼저 나온 손만 인식
     cv2.imshow('', img)
     if cv2.waitKey(1) & 0xFF == 27:
         break
