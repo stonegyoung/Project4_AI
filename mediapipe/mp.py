690d51aaabb8b3c99752cb9a466a267fa7849b0a
seok <tjralsrud8@gmail.com>
2024-10-17 10:57:14 +0900

    mp.py 수정1

--- mediapipe/mp.py
+++ mediapipe/mp.py
@@ -13,7 +13,6 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
 while cap.isOpened():
-    point_x, point_y = None, None
     ret, img = cap.read()
     img = cv2.flip(img, 1)
 
@@ -39,9 +38,8 @@ while cap.isOpened():
             else:
                 print({"lr": 'Right', "x": point_x, "y": point_y})
             
-
-            
         # print({"lr": hand_lr.classification[0].label, "x": point_x, "y": point_y}) # 먼저 나온 손만 인식
+        
     cv2.imshow('', img)
     if cv2.waitKey(1) & 0xFF == 27:
         break
