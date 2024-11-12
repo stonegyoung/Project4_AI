11cd9691ee92bfa276670035ff134f36bc1cd89a
seok <tjralsrud8@gmail.com>
2024-11-12 17:51:47 +0900

    QnA 폴더 설명

--- QnAchat/qna.py
+++ QnAchat/qna.py
@@ -1,6 +1,6 @@
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts.few_shot import FewShotPromptTemplate
 from langchain_core.prompts import PromptTemplate
