8403e6b4fda1d1f06feef33a450d5a9344ac283b
seok <tjralsrud8@gmail.com>
2024-11-12 14:58:13 +0900

    quiz 서버 수정

--- server.py
+++ server.py
@@ -6,9 +6,10 @@ from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain.schema import SystemMessage
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts.few_shot import FewShotPromptTemplate
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableMap, RunnablePassthrough
+import random
 
 import pyrebase
 import json
@@ -83,44 +84,27 @@ chat_messages = [
 chat_prompt = ChatPromptTemplate.from_messages(chat_messages)
 
 ################################################################################################################################################################
-# 질문 예시
+# 질문과 정답
 examples = [
     {
         "question": "드라마의 특성으로 거리가 먼 것은?",
-        "n1" :"장소의 제한을 거의 받지 않는다.",
-        "n2" :"음악을 통하여 분위기를 알 수 있다.",
-        "n3" :"주로 문자를 통하여 내용이 전달된다.",
-        "n4" :"연출가, 작가, 배우 등 여러 사람이 함께 만든다.",
-        "answer" :"3"
-    },
-    {
-        "question": "독도가 대한민국 영토임을 알리는 이유로 알맞은 것은",
-        "n1": "독도가 일본 본토와 가깝기 때문이다.",
-        "n2": "독도는 중요한 자원이 많아 경제적으로 중요한 지역이다.",
-        "n3": "독도가 오랫동안 대한민국의 행정 구역으로 관리되어 왔기 때문이다.",
-        "n4": "독도에는 대한민국의 유명한 관광지가 있기 때문이다.",
-        "answer": "3"
+        "answer" :"주로 문자를 통하여 내용이 전달된다.",
     },
     {
         "question": "소설 *소나기*에서 소년이 소녀에게 특별한 감정을 느끼게 된 계기로 거리가 가까운 것은?",
-        "n1": "둘이 함께 소나기를 피하면서",
-        "n2": "소녀가 소년에게 꽃다발을 주면서",
-        "n3": "소년이 소녀의 생일에 선물을 주면서",
-        "n4": "소녀가 학교에서 발표를 잘해서",
-        "answer": "1"
+        "answer": "둘이 함께 소나기를 피하면서"
     }
 ]
 example_prompt = PromptTemplate.from_template(
-    "'문제' : '{question}', 'n1': '{n1}', 'n2': '{n2}', 'n3': '{n3}', 'n4': '{n4}', '정답': '{answer}'"
+    "'ques' : '{question}', 'ans': '{answer}'" # examples랑 같아야 함
 )
 qna_prompt = FewShotPromptTemplate(
     examples=examples,
     example_prompt=example_prompt,
-    prefix = "당신은 선생님입니다. {context}를 기반으로 정확하게 {theme}에 대한 문제를 하나 만들고 4가지 선다와 정답을 JSON 형식으로 만들어주세요. 정답이 없을 수는 없고, 정답이 아닌 선지는 누가 봐도 답이 아니라고 생각 해야 합니다.",
-    suffix="'문제': '생성된 문제', 'n1': '선택지 1', 'n2': '선택지 2', 'n3': '선택지 3', 'n4': '선택지 4', '정답': '정답 번호'",
+    prefix = "당신은 선생님입니다. {context}를 기반으로 정확하게 {theme}에 대한 객관식으로 선택할 수 있는 문제를 하나 만들고 질문에 대한 정답을 JSON 형식으로 만들어주세요. 문제는 이의 제기가 일어나지 않게 '거리가 가까운 것은?', '거리가 먼 것은?'이라는 형태로 끝나야 합니다.",
+    suffix="'ques': '생성된 문제', 'ans': '정답'",
     input_variables=["theme"],
 )
-
 rag_chain = (
     {'context': retriever|format_docs, 'theme': RunnablePassthrough()} # chat_prompt가 갖는 dict
     |qna_prompt # 프롬프트
@@ -128,6 +112,45 @@ rag_chain = (
 )
 
 theme_list = ["해양쓰레기 발생원인", "해양쓰레기 현황", "해양쓰레기 피해 및 위험성", "해양쓰레기 피해 사례", "태평양 해양 쓰레기 섬", "미세 플라스틱", "허베이스피릿호 원유유출 사고", "검은 공 사건", "약품 사고", "제주 바다 돌고래", "바다 거북", "상괭이"]
+
+
+# 오답
+val_examples = [
+    {
+        "question":"해양쓰레기 종류에 대한 설명으로 알맞은 것은?",
+        "answer": "해양쓰레기는 육상과 해상 모두에서 발생할 수 있다.",
+        "corr_1": "해양쓰레기는 오직 플라스틱으로만 구성된다.",
+        "corr_2": "해양쓰레기는 바다 생물의 생태계에 직접적인 영향을 미치지 않는다.",
+        "corr_3": "해양쓰레기는 모두 수거되어 재활용된다.",
+    },
+    {
+        "question": "해양쓰레기가 발생하는 주된 원인은 무엇인가?",
+        "answer": "하천과 강을 따라 바다로 들어오는 쓰레기", 
+        "corr_1": "해양 동물의 생태계를 보호하기 위한 법제정", 
+        "corr_2": "멸종 위기 종의 증가",
+        "corr_3": "자원봉사자의 봉사활동",
+    }
+]
+val_example_prompt = PromptTemplate.from_template(
+    "문제: {question}\n정답: {answer}\n"
+    "오답: 'wrong1': '{corr_1}', 'wrong2': '{corr_2}', 'wrong3': '{corr_3}'"
+)
+
+valid_prompt = FewShotPromptTemplate(
+    examples=val_examples,
+    example_prompt=val_example_prompt,
+    prefix="당신은 다음의 문제를 보고, 문제에 대한 오답 선지를 만드는 사람입니다. 오해의 소지가 있을 문구는 제외하여 오답을 만들고 JSON 형식으로 만들어주세요.",
+    suffix= "문제: {ques}\n정답: {ans}\n오답: ",
+    input_variables=["ques", "ans"],
+)
+
+val_chain = (
+    RunnableMap({
+        'ques': RunnablePassthrough(),
+        'ans': RunnablePassthrough(),
+    })
+    |valid_prompt | chatgpt 
+)
 ################################################################################################################################################################
 
 
@@ -229,7 +252,18 @@ async def qna():
     n = np.random.randint(0,len(theme_list))
     ans = await rag_chain.ainvoke(theme_list[n])
     js = convert_json(ans.content)
-    return js
+    
+    ans = val_chain.invoke(js)
+    wrong = convert_json(ans.content)
+    
+    js.update(wrong)
+
+    options = [js['ans'], js['wrong1'], js['wrong2'], js['wrong3']]
+    random.shuffle(options)
+
+    quiz = {"문제": js['ques'], "n1":options[0], "n2":options[1], "n3": options[2], "n4": options[3], "정답": options.index(js['ans'])+1}
+
+    return quiz
 
 @app.get("/testchatbot")
 def testchatbot(tc:Chat):
